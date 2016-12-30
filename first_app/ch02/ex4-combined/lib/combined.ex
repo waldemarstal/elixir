081dce352d07ddc3fd09df3e3950306da710f7fe
waldest <waldek_15@o2.pl>
2016-12-30 22:31:11 +0100

    after ch02

---
+++ first_app/ch02/ex4-combined/lib/combined.ex
@@ -0,0 +1,11 @@
+# defmodule Combined do
+#   def height_to_mph(meters) do
+#     Convert.mps_to_mph(Drop.fall_velocity(meters))
+#   end
+# end
+
+defmodule Combined do
+  def height_to_mph(meters) do
+    Drop.fall_velocity(meters) |> Convert.mps_to_mph
+  end
+end
