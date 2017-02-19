12ed946e233b95d76704f4ad875efaea7f5961a6
waldest <waldek_15@o2.pl>
2017-02-19 11:59:31 +0100

    chapter 11

---
+++ first_app/ch10/lib/drop.ex
@@ -0,0 +1,31 @@
+require Logger
+
+defmodule Drop do
+  def fall_velocity(planemo, distance) do
+    Logger.info("Danger function")
+    try do
+      gravity = case planemo do
+        :earth -> 9.8
+        :moon -> 1.6
+        :mars -> 3.71
+      end
+      :math.sqrt(2 * gravity * distance)
+    rescue
+      error -> error
+    end
+  end
+
+  def fall_velocity2(planemo, distance) do
+    try do
+      gravity = case planemo do
+        :earth -> 9.8
+        :moon -> 1.6
+        :mars -> 3.71
+      end
+      :math.sqrt(2 * gravity * distance)
+    rescue
+      ArithmeticError -> {:error, "Distance must be non-negative"}
+      CaseClauseError -> {:error, "Unknown planemo #{planemo}"}
+    end
+  end
+end
